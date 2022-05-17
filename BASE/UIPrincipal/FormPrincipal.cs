f3a7b9ec256f76c7c899ce6a56f457d0deaa62b7
Bruno Alencar Moreira <90804306+BMoreiraa@users.noreply.github.com>
2022-05-16 22:04:24 -0300

    aula
    aula 16/05/2022

---
+++ BASE/UIPrincipal/FormPrincipal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIPrincipal
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void grupoDeUsuarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void usuáriosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (FormconsultaUsuário frm = new FormconsultaUsuário())
+            {
+                frm.ShowDialog();
+            }
+        }
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
